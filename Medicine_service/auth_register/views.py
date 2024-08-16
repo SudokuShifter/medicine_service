aee3e32985d838503a451bbf3aaf241d9bffc28d
mr.Robot <zanid227@yandex.ru>
2024-08-16 17:12:04 +0300

    add model manager for Account and create FormRegistration

--- Medicine_service/auth_register/views.py
+++ Medicine_service/auth_register/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from .models import Account
+from .forms import RegisterForm
 # Create your views here.
+
+
+class RegisterView(CreateView):
+    model = Account
+    form_class = RegisterForm
+    template_name = 'auth_register/register.html'
+    success_url = reverse_lazy('home')
