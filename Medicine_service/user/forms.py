5c295c719f12b188e13b079428e3daa93153614b
mr.Robot <zanid227@yandex.ru>
2024-08-22 01:07:57 +0300

    work in process

--- Medicine_service/user/forms.py
+++ Medicine_service/user/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.core.exceptions import ValidationError
 
-from .models import User
+from .models import User, Address
 import datetime
 
 
@@ -13,9 +13,9 @@ class UserData(forms.ModelForm):
             'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Имя'}),
             'second_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Фамилия'}),
             'middle_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Отчество'}),
-            'birthday': forms.DateInput(attrs={'class': 'form-control', 'placeholder': 'Дата рождения'}),
+            'birthday': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),  # HTML5 date picker
             'address': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Адрес'}),
-            'Photo': forms.ClearableFileInput(attrs={'class': 'form-control', 'placeholder': 'Фото'}),
+            'photo': forms.ClearableFileInput(attrs={'class': 'form-control-file'}),
         }
 
     def clean_birthday(self):
@@ -23,3 +23,16 @@ class UserData(forms.ModelForm):
         if birthday > datetime.date.today():
             raise ValidationError('Введите корректную дату рождения')
         return birthday
+
+
+class UserAddressForm(forms.ModelForm):
+    class Meta:
+        model = Address
+        fields = ['country', 'city', 'street', 'house_number', 'flat_number']
+        widgets = {
+            'country': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Страна'}),
+            'city': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Город'}),
+            'street': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Улица'}),
+            'house_number': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Номер дома'}),
+            'flat_number': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Номер квартиры'}),
+        }
