fb364cd93adcaaf9f3a4e1103d4e1dd28da90512
SudokuShifter <158267096+SudokuShifter@users.noreply.github.com>
2024-08-28 19:36:07 +0300

    Delete Medicine_service/user directory

--- Medicine_service/user/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('home/', views.UserHome.as_view(), name='home'),
-    path('profile/<slug:slug>', views.UserProfileView.as_view(), name='profile'),
-    path('edit_data/<slug:slug>', views.UserUpdateView.as_view(), name='edit_data'),
-]
