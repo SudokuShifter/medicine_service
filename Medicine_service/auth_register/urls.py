db5062a09879f2b0f8292292e90983c4d4aad694
SudokuShifter <158267096+SudokuShifter@users.noreply.github.com>
2024-08-28 19:35:52 +0300

    Delete Medicine_service/auth_register directory

--- Medicine_service/auth_register/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('register/', views.RegisterView.as_view(), name='register'),
-    path('auth/', views.AuthView.as_view(), name='auth'),
-    path('logout/', views.Logout.as_view(), name='logout')
-]
