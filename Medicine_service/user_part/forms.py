97e65ae8ca2e30ddb77945315d61087126475293
mr.Robot <zanid227@yandex.ru>
2024-09-05 19:50:11 +0300

    Доработал форму регистрации юзера, чтобы её можно было удобно опрокинуть в темплейт через теги

--- Medicine_service/user_part/forms.py
+++ Medicine_service/user_part/forms.py
@@ -1,12 +1,18 @@
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from django import forms
 
 
 class CustomCreateUserForm(UserCreationForm):
     class Meta:
         fields = [
-            'first_name', 'email', 'phone', 'password1', 'password2'
+            'first_name', 'email', 'password1', 'password2'
         ]
+        widgets = {
+            'first_name': forms.TextInput(attrs={'placeholder': 'Иван Иванов'}),
+            'email': forms.TextInput(attrs={'placeholder': 'name@example.com'}),
+            'password1': forms.PasswordInput(attrs={'placeholder': 'Пароль'}),
+            'password2': forms.PasswordInput(attrs={'placeholder': 'Пароль'}),
+        }
         labels = {
             'first_name': 'Имя',
             'email': 'Email',
