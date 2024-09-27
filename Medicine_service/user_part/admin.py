9721b22db1353092615e22281f8f68fd24df658a
mr.Robot <zanid227@yandex.ru>
2024-09-27 16:52:39 +0300

    Доработал отображение контекста в темплейтах

--- Medicine_service/user_part/admin.py
+++ Medicine_service/user_part/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Position
 # Register your models here.
+
+
+@admin.register(Position)
+class PositionAdmin(admin.ModelAdmin):
+    fields = ['title', 'description']
