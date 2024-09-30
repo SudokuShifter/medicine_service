8c3522801df2950c942c9831e42ecf0a1d53d919
mr.Robot <zanid227@yandex.ru>
2024-09-30 18:05:59 +0300

    Пересмотрел структуру проекта и решил выбрать другой подход для формирования моделей записи

--- Medicine_service/records/forms.py
+++ Medicine_service/records/forms.py
@@ -1,17 +1,3 @@
 from django import forms
 
-from .models import ScheduleDoctor
-
-
-class CreateScheduleDoctor(forms.ModelForm):
-    class Meta:
-        model = ScheduleDoctor
-        fields = [
-            'start_at', 'end_at'
-        ]
-        widgets = {
-            'start_at': forms.DateTimeInput(attrs={'type': 'datetime'}),
-            'end_at': forms.DateTimeInput(attrs={'type': 'datetime'}),
-        }
-
 
--- Medicine_service/records/views.py
+++ Medicine_service/records/views.py
@@ -1,12 +1,2 @@
 from django.shortcuts import render
 from django.views.generic import CreateView
-
-from .models import ScheduleDoctor
-from .forms import CreateScheduleDoctor
-# Create your views here.
-
-
-class CreateSchedule(CreateView):
-    model = ScheduleDoctor
-    form_class = CreateScheduleDoctor
-    # template_name =
