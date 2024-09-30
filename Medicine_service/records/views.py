626bc372866fba3b5cb6ff1037f87d569f7f0ad1
mr.Robot <zanid227@yandex.ru>
2024-09-30 11:52:48 +0300

    Сделал поле slug для моделей юзера и доктора уникальным

--- Medicine_service/records/views.py
+++ Medicine_service/records/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
 
+from .models import ScheduleDoctor
+from .forms import CreateScheduleDoctor
 # Create your views here.
+
+
+class CreateSchedule(CreateView):
+    model = ScheduleDoctor
+    form_class = CreateScheduleDoctor
+    # template_name =
