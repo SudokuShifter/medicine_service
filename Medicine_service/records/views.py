17cce3651f988a8d48779d43cef2fa502aea9f2c
mr.Robot <zanid227@yandex.ru>
2024-10-04 17:06:03 +0300

    Добавил возможность менять данные юзера по слагу и начал реализацию формы для записи

--- Medicine_service/records/views.py
+++ Medicine_service/records/views.py
@@ -52,9 +52,10 @@ class RateDoctorView(View):
         return redirect('doc_list')
 
 
-class CreateRecord(BaseCreateView):
+class CreateRecord(CreateView):
     model = PatientRecord
     form_class = RecordForm
+    template_name = 'record_form.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
