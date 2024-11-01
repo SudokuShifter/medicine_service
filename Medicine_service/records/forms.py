7b833cd35ad16e619475027fe68df5d35a8a18b9
mr.Robot <zanid227@yandex.ru>
2024-11-01 19:53:47 +0300

    Продолжаю документацию

--- Medicine_service/records/forms.py
+++ Medicine_service/records/forms.py
@@ -4,6 +4,10 @@ from .models import PatientRecord
 
 
 class RecordForm(forms.ModelForm):
+    """
+    Форма для записи пациента к врачу. Наслудется от forms.ModelForm (базовая штука джанги)
+    Класс Meta внутри сделан для удобства.
+    """
     class Meta:
         model = PatientRecord
         fields = ['appointment_date', 'description_patient', 'description']
